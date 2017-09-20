f9eaa764e7434d2b8b77eb4875bb585e204dbc77
Yu <maoyu960320@hotmail.com>
2017-09-20 17:10:36 +0800

    update

--- List/List.js
+++ List/List.js
@@ -1,55 +1,26 @@
-function List() {
-    this.listSize = 0;
-    this.pos = 0;
-    this.dataStore = [];
-    this.clear = clear;
-    this.find = find;
-    this.toString = toString;
-    this.insert = insert;
-    this.append = append;
-    this.remove = remove;
-    this.length = length;
-    this.contains = contains;
-}
-
-function append(el) {
-    this.dataStore[this.listSize++] = el;
-}
-
-function find(el) {
-    return this.dateStore.indexOf(el);
-}
-
-function remove(el) {
-    const elPos = this.find(el);
-    if (elPos > -1) {
-        this.dataStore.splice(elPos, 1);
-        --this.listSize;
-        return true;
-    }
-    return false;
-}
-
-function length() {
-    return this.listSize;
-}
-
-function toString() {
-    return this.dataStore;
-}
-
-function insert(el, pos) {
-    this.dataStore.splice(el, 0, pos);
-    ++this.listSize;
-    return true;
-}
-
-function clear() {
-    delete this.dataStore;
-    this.dataStore = [];
-    this.listSize = 0;
-}
-
-function contains(el) {
-    return this.dataStore.includes(el);
+function LinkedList() {
+    let Node = function (element) {
+        this.element = element;
+        this.next = null;
+    };
+    let length = 0;
+    let head = null;
+    this.append = function (element) {
+    };
+    this.insert = function (position, element) {
+    };
+    this.removeAt = function (position) {
+    };
+    this.remove = function (element) {
+    };
+    this.indexOf = function (element) {
+    };
+    this.isEmpty = function () {
+    };
+    this.size = function () {
+    };
+    this.toString = function () {
+    };
+    this.print = function () {
+    };
 }
